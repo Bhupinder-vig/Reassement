4f0898e0dd745edc785080043dff30d9f6ae8ab1
Bhupinder Singh Vig <40711522@live.napier.ac.uk>
2026-07-31 08:50:23 +0100

    Define City class with attributes
    Added fields to represent city attributes such as ID, name, country code, district, population, and country.

--- src/main/java/com/napier/sem/City.java
+++ src/main/java/com/napier/sem/City.java
@@ -1,4 +1,38 @@
 package com.napier.sem;
 
+/**
+ * Represents A City
+ */
 public class City {
+
+    /**
+     * City ID
+     */
+    public int id;
+
+    /**
+     * City Name
+     */
+    public String name;
+
+    /**
+     * Country Code
+     */
+    public String country_code;
+
+    /**
+     * City District Name
+     */
+    public String district;
+
+    /**
+     * City Population
+     */
+    public int population;
+
+    /**
+     * Country Name
+     */
+    public String country;
+
 }
