a213ca0c017d1d8c30019f5403719f1c7db8e541
bhupinder-vig <40711522@live.napier.ac.uk>
2026-07-31 14:36:59 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/napier/sem/App.java

---
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -0,0 +1,32 @@
+package com.napier.sem;
+
+/**
+ * Represents A Country Language
+ */
+public class CountryLanguage {
+
+    /**
+     * Country Code
+     */
+    public String code;
+
+    /**
+     * Country Language
+     */
+    public String language;
+
+    /**
+     * Country Language Is Official?
+     */
+    public String is_official;
+
+    /**
+     * Country Language Total Speakers
+     */
+    public int total_speakers;
+
+    /**
+     * Country Language Percentage
+     */
+    public double percentage;
+}
